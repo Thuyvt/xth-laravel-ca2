e27289c0608241ede5011c574eb8f0649b9c8629
thuyvt <thuyvt1005@gmail.com>
2024-07-26 09:45:09 +0700

    buổi 5

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Product;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $products = Product::query()->latest('id')->limit(30)->get();
+    return view('welcome',compact('products'));
 });
 
